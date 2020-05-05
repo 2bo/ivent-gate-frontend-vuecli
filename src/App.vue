b11dcd64858857b55620b9a7f43233f5e5790142
2bo <developer.2bo@gmail.com>
2020-05-05 16:08:41 +0900

    add temp searched event list and pagination

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,28 @@
     <v-app>
         <AppHeader @search="onSearch"></AppHeader>
         <v-content>
-            <HelloWorld/>
+            <v-container>
+                <v-row justify="center">
+                    <v-col cols="7" v-for="event in events" :key="event.id">
+                        <EventCard :title="event.title"
+                                   :catch_text="event.catch"
+                                   :started_at="event.started_at"
+                                   :event_url="event.event_url"
+                                   :description="event.description">
+
+                        </EventCard>
+                    </v-col>
+                    <v-col cols="8">
+                        <v-container class="max-width">
+                            <v-pagination
+                                    v-model="current_page"
+                                    :length="last_page"
+                                    @input="searchEvents"
+                            ></v-pagination>
+                        </v-container>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-content>
     </v-app>
 </template>
@@ -10,12 +31,15 @@
 <script>
     import axios from "axios";
     import AppHeader from './components/AppHeader';
+    import EventCard from './components/EventCard'
+
 
     export default {
         name: 'App',
 
         components: {
             AppHeader,
+            EventCard
         },
 
         data: () => ({
