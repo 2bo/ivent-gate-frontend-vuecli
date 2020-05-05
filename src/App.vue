5fb1cce839291622fc1d736d598056766d36b06d
2bo <developer.2bo@gmail.com>
2020-05-05 16:00:05 +0900

    add search events method

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,45 @@
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld';
+    import axios from "axios";
     import AppHeader from './components/AppHeader';
 
     export default {
         name: 'App',
 
         components: {
-            HelloWorld,
             AppHeader,
         },
 
         data: () => ({
-            events: []
+            events: [],
+            places: [],
+            types: [],
+            keywords: "",
+            current_page: 1,
+            last_page: 0
         }),
 
         methods: {
-            onSearch(results) {
-                this.events = results;
+            onSearch: function (params) {
+                this.places = params.places;
+                this.types = params.types;
+                this.keywords = params.keywords;
+                this.current_page = 1;
+                this.searchEvents()
+            },
+            searchEvents: async function () {
+                const response = await axios.get('/events/search', {
+                    params: {
+                        places: this.places,
+                        types: this.types,
+                        keywords: this.keywords,
+                        page: this.current_page
+                    }
+                });
+                this.events = response.data.data;
+                this.current_page = response.data.current_page;
+                this.last_page = response.data.last_page;
             }
         }
     };
