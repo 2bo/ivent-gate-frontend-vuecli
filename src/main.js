48ec31fac8a57a2f2e01a247c9500f239ec44891
2bo <developer.2bo@gmail.com>
2020-05-21 23:11:09 +0900

    add vuex store file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
+import store from './store'
 
 Vue.config.productionTip = false;
 // FIXME: fix url for production
@@ -11,5 +12,6 @@ axios.defaults.baseURL = 'http://localhost:10080/api';
 new Vue({
     vuetify,
     router,
+    store,
     render: h => h(App)
 }).$mount('#app');
---
+++ src/store.js
@@ -0,0 +1,103 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import eventsApi from './api/eventsApi'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        types: [],
+        places: [],
+        keywords: "",
+        page: 1,
+        last_page: null,
+        events: [],
+        isLoading: false
+    },
+    mutations: {
+        setTypes(state, types) {
+            if (Array.isArray(types)) {
+                state.types = types
+            }
+        },
+        setPlaces(state, places) {
+            if (Array.isArray(places)) {
+                state.places = places
+            }
+        },
+        setKeywords(state, keywords) {
+            state.keywords = keywords
+        },
+        setPage(state, page) {
+            if (!Number.isNaN(page)) {
+                state.page = page
+            }
+        },
+        setLastPage(state, lastPage) {
+            if (!Number.isNaN(lastPage)) {
+                state.last_page = lastPage
+            }
+        },
+        setEvents(state, events) {
+            state.events = events
+        },
+        setLoading(state, isLoading) {
+            state.isLoading = isLoading
+        }
+    },
+    getters: {
+        getTypes(state) {
+            return state.types
+        },
+        getPlaces(state) {
+            return state.places
+        },
+        getKeywords(state) {
+            return state.keywords
+        },
+        getPage(state) {
+            return state.page
+        },
+        getLastPage(state) {
+            return state.last_page
+        },
+        getEvents(state) {
+            return state.events
+        },
+        isLoading(state) {
+            return state.isLoading
+        }
+    },
+    actions: {
+        async searchEvents({commit, getters}) {
+            commit('setLoading', true);
+            const params = {
+                places: getters.getPlaces,
+                types: getters.getTypes,
+                keywords: getters.getKeywords,
+                page: 1
+            };
+            const response = await eventsApi.searchEvents(params);
+            commit('setPage', 1);
+            commit('setEvents', response.data.data);
+            commit('setLastPage', response.data.last_page);
+            commit('setLoading', false);
+
+        },
+        async pagingEvents({commit, getters}) {
+            commit('setLoading', true);
+            const params = {
+                places: getters.getPlaces,
+                types: getters.getTypes,
+                keywords: getters.getKeywords,
+                page: getters.getPage
+            };
+            const response = await eventsApi.searchEvents(params);
+            commit('setEvents', response.data.data);
+            commit('setLastPage', response.data.last_page);
+            commit('setLoading', false);
+        }
+    }
+});
+
+export default store;
